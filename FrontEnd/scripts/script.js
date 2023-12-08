753152621090b441a2930021f1af15b0bd99c516
Zeldanyan <pelucheneko@gmail.com>
2023-12-09 00:19:18 +0100

    maj js

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -8,11 +8,15 @@ async function get_api(link) {
 }
 
 // function pour les projets
-async function f_work() {
+// category, id 1 = "Objets"
+// category, id 2 = "Appartements"
+// category, id 3 = "Hotels & restaurants"
+async function f_work(filtre) {
     const gallery = document.querySelector(".gallery");
     const work = await get_api("works");
 
-    let i = 0;
+    gallery.innerHTML = ""
+    i = 0;
     while (i < work.length) {
         gallery.innerHTML += "<figure><img src=" + work[i].imageUrl + " alt="+ work[i].title +"><figcaption>"+ work[i].title +"</figcaption></figure>";
         i++;
@@ -21,4 +25,12 @@ async function f_work() {
 
 //doc api = http://localhost:5678/api-docs/
 const api = "http://localhost:5678/api/";
+const bouton_filtre = [document.getElementById("menu_all"),document.getElementById("menu_objects"),document.getElementById("menu_apparts"),document.getElementById("menu_HR")];
+let i = 0;
+
 f_work();
+i = 0;
+while (i < bouton_filtre.length) {
+    bouton_filtre[i].addEventListener("click", f_work(bouton_filtre[i]))
+    i++;
+}
