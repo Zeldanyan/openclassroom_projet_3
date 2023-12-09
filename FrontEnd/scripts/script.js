f67edb340edfe24fa671b83b7735b458d6d6f3dc
Zeldanyan <pelucheneko@gmail.com>
2023-12-09 01:05:35 +0100

    filtre fonctionnels

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -18,19 +18,32 @@ async function f_work(filtre) {
     gallery.innerHTML = ""
     i = 0;
     while (i < work.length) {
-        gallery.innerHTML += "<figure><img src=" + work[i].imageUrl + " alt="+ work[i].title +"><figcaption>"+ work[i].title +"</figcaption></figure>";
+        if (filtre === null || filtre.value === 0) {
+            gallery.innerHTML += "<figure><img src=" + work[i].imageUrl + " alt="+ work[i].title +"><figcaption>"+ work[i].title +"</figcaption></figure>";
+        } else if (filtre.value === 1 && work[i].category.id === 1) {
+            gallery.innerHTML += "<figure><img src=" + work[i].imageUrl + " alt="+ work[i].title +"><figcaption>"+ work[i].title +"</figcaption></figure>";
+        } else if (filtre.value === 2 && work[i].category.id === 2) {
+            gallery.innerHTML += "<figure><img src=" + work[i].imageUrl + " alt="+ work[i].title +"><figcaption>"+ work[i].title +"</figcaption></figure>";
+        } else if (filtre.value === 3 && work[i].category.id === 3) {
+            gallery.innerHTML += "<figure><img src=" + work[i].imageUrl + " alt="+ work[i].title +"><figcaption>"+ work[i].title +"</figcaption></figure>";
+        }
         i++;
     }
 }
 
 //doc api = http://localhost:5678/api-docs/
 const api = "http://localhost:5678/api/";
-const bouton_filtre = [document.getElementById("menu_all"),document.getElementById("menu_objects"),document.getElementById("menu_apparts"),document.getElementById("menu_HR")];
+const bouton_filtre = [
+    document.getElementById("menu_all"),
+    document.getElementById("menu_objects"),
+    document.getElementById("menu_apparts"),
+    document.getElementById("menu_HR")
+];
 let i = 0;
 
-f_work();
-i = 0;
-while (i < bouton_filtre.length) {
-    bouton_filtre[i].addEventListener("click", f_work(bouton_filtre[i]))
-    i++;
-}
+f_work(null);
+
+bouton_filtre[0].addEventListener("click", function() {f_work(bouton_filtre[0]);});
+bouton_filtre[1].addEventListener("click", function() {f_work(bouton_filtre[1]);});
+bouton_filtre[2].addEventListener("click", function() {f_work(bouton_filtre[2]);});
+bouton_filtre[3].addEventListener("click", function() {f_work(bouton_filtre[3]);});
