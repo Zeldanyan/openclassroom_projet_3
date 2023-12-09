43aef1823f80fc6f7c82e8e2321e088a8ddfbaa8
Zeldanyan <pelucheneko@gmail.com>
2023-12-09 01:36:03 +0100

    filtre css

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -8,23 +8,31 @@ async function get_api(link) {
 }
 
 // function pour les projets
-// category, id 1 = "Objets"
-// category, id 2 = "Appartements"
-// category, id 3 = "Hotels & restaurants"
 async function f_work(filtre) {
     const gallery = document.querySelector(".gallery");
     const work = await get_api("works");
 
+    //maj class du filtre
+    i = 0;
+    if (filtre !== null) {
+        while (i < bouton_filtre.length) {
+            bouton_filtre[i].classList.remove("select");
+            i++;
+        }
+        filtre.classList.add("select");
+    }
+
+    // generer les travaux avec et sans les filtres
     gallery.innerHTML = ""
     i = 0;
-    while (i < work.length) {
+    while (i < work.length) { 
         if (filtre === null || filtre.value === 0) {
             gallery.innerHTML += "<figure><img src=" + work[i].imageUrl + " alt="+ work[i].title +"><figcaption>"+ work[i].title +"</figcaption></figure>";
-        } else if (filtre.value === 1 && work[i].category.id === 1) {
+        } else if (filtre.value === 1 && work[i].category.id === 1) { // category, id 1 = "Objets"
             gallery.innerHTML += "<figure><img src=" + work[i].imageUrl + " alt="+ work[i].title +"><figcaption>"+ work[i].title +"</figcaption></figure>";
-        } else if (filtre.value === 2 && work[i].category.id === 2) {
+        } else if (filtre.value === 2 && work[i].category.id === 2) { // category, id 2 = "Appartements"
             gallery.innerHTML += "<figure><img src=" + work[i].imageUrl + " alt="+ work[i].title +"><figcaption>"+ work[i].title +"</figcaption></figure>";
-        } else if (filtre.value === 3 && work[i].category.id === 3) {
+        } else if (filtre.value === 3 && work[i].category.id === 3) { // category, id 3 = "Hotels & restaurants"
             gallery.innerHTML += "<figure><img src=" + work[i].imageUrl + " alt="+ work[i].title +"><figcaption>"+ work[i].title +"</figcaption></figure>";
         }
         i++;
@@ -42,7 +50,6 @@ const bouton_filtre = [
 let i = 0;
 
 f_work(null);
-
 bouton_filtre[0].addEventListener("click", function() {f_work(bouton_filtre[0]);});
 bouton_filtre[1].addEventListener("click", function() {f_work(bouton_filtre[1]);});
 bouton_filtre[2].addEventListener("click", function() {f_work(bouton_filtre[2]);});
