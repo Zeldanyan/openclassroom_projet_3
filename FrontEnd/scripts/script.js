0da75ecd8891ab50a868777ab426cdd619bd93fd
Zeldanyan <pelucheneko@gmail.com>
2024-01-04 23:37:15 +0100

    maj soutenance

--- FrontEnd/scripts/script.js
+++ FrontEnd/scripts/script.js
@@ -264,6 +264,8 @@ if (token !== null) { // verifier le token
     btn_log.textContent = "logout";
     btn_log.href = "#";
     btn_edit.style.display = "flex";
+    document.querySelector(".menu").style.display = "none";
+    document.querySelector("body").style.paddingTop = "59px";
 }
 
 // log out
